686cc499ae69d78184d34186af7827e07fc2c97f
Raotalha321 <raotalha333221@gmail.com>
2025-12-18 04:08:22 +0500

    Add new Cypress test in spec.cy.js

--- e2e/spec.cy.js
+++ e2e/spec.cy.js
@@ -3,7 +3,7 @@ describe('template spec', () => {
     cy.visit('https://dashboard.stripe.com/login')
     cy.get('[data-testid="login-email-input"]').click() .type('talhatanveer568@gmail.com.com')
     cy.get('[data-testid="login-password-input"]').click().type('@Green333221')
-    cy.get('.Button-align > .Flex-flex').click()
+    cy.get('.Button-align > .Flex-flex').click().type()
 
   })
 })
